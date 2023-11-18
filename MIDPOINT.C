3eff91cd5a1ab4f56f3066f631a7aa8ebed7a724
codemaster <145989639+codemater1234@users.noreply.github.com>
2023-11-18 13:52:33 +0530

    Update MIDPOINT.C

--- MIDPOINT.C
+++ MIDPOINT.C
@@ -1,55 +1,37 @@
 #include <stdio.h>
 #include <graphics.h>
+#include <math.h>
+#include <conio.h>
+void drawCircle(int x0, int y0, int radius, int points) {
+    float angle, x, y;
 
-void drawCircle(int x0, int y0, int radius) {
-    int y = radius;
-    int x = 0;
-    int p = 1 - radius;
+    for (int i = 0; i < points; i++) {
+        angle = 2 * M_PI * i / points;
+        x = x0 + radius * cos(angle);
+        y = y0 + radius * sin(angle);
 
-    while (x <= y) {
-        putpixel(x0 + x, y0 + y, 15);
-        delay(10);
-        putpixel(x0 + y, y0 + x, 15);
-        delay(10);
-        putpixel(x0 - x, y0 + y, 15);
-        delay(10);
-        putpixel(x0 - y, y0 + x, 15);
-        delay(10);
-        putpixel(x0 - x, y0 - y, 15);
-        delay(10);
-        putpixel(x0 - y, y0 - x, 15);
-        delay(10);
-        putpixel(x0 + x, y0 - y, 15);
-        delay(10);
-        putpixel(x0 + y, y0 - x, 15);
-        delay(10);
-
-        if (p < 0) {
-            x++;
-            p += 2 * x + 1;
-        } else {
-            x++;
-            y--;
-            p += 2 * (x - y) + 1;
-        }
+        putpixel((int)x, (int)y, 15);
     }
 }
 
 int main() {
-    int gd = DETECT, gm, x, y, r;
+    int gd = DETECT, gm, x, y, radius, points;
 
     initgraph(&gd, &gm, "C://TURBOC3//BGI");
 
     printf("Enter radius: ");
-    scanf("%d", &r);
+    scanf("%d", &radius);
+
+    printf("Enter number of points: ");
+    scanf("%d", &points);
 
     printf("Enter coordinates (x y): ");
     scanf("%d %d", &x, &y);
 
-    drawCircle(x, y, r);
+    drawCircle(x, y, radius, points);
 
-    getch();
+    getch();  // Wait for a key press before closing the graphics window
+
     closegraph();
-
     return 0;
 }
