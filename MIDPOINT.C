cc8e2e2b4fc45fe8328492c81234e1144771d87b
codemaster <145989639+codemater1234@users.noreply.github.com>
2023-11-18 17:12:48 +0530

    Update MIDPOINT.C

--- MIDPOINT.C
+++ MIDPOINT.C
@@ -1,37 +1,66 @@
 #include <stdio.h>
 #include <graphics.h>
-#include <math.h>
-#include <conio.h>
-void drawCircle(int x0, int y0, int radius, int points) {
-    float angle, x, y;
+ #include <conio.h>
+void drawCircle(int xc, int yc, int x, int y)
+{
+    putpixel(xc + x, yc + y, 15);
+    putpixel(xc - x, yc + y, 15);
+    putpixel(xc + x, yc - y, 15);
+    putpixel(xc - x, yc - y, 15);
+    putpixel(xc + y, yc + x, 15);
+    putpixel(xc - y, yc + x, 15);
+    putpixel(xc + y, yc - x, 15);
+    putpixel(xc - y, yc - x, 15);
+}
 
-    for (int i = 0; i < points; i++) {
-        angle = 2 * M_PI * i / points;
-        x = x0 + radius * cos(angle);
-        y = y0 + radius * sin(angle);
+void midPointCircle(int xc, int yc, int radius)
+{
+    int x = 0, y = radius;
+    int p = 1 - radius;
 
-        putpixel((int)x, (int)y, 15);
+    // Initial point on the axes
+    drawCircle(xc, yc, x, y);
+
+    // When radius is zero, only a single point will be printed at the center
+    if (radius > 0)
+    {
+        // Initial Decision Parameter of the region 1
+        while (x < y)
+        {
+            x++;
+
+            // Mid-point is inside or on the perimeter of the circle
+            if (p < 0)
+                p = p + 2 * x + 1;
+
+            // Mid-point is outside the perimeter of the circle
+            else
+            {
+                y--;
+                p = p + 2 * (x - y) + 1;
+            }
+
+            // All the perimeter points have already been printed
+            drawCircle(xc, yc, x, y);
+        }
     }
 }
 
-int main() {
-    int gd = DETECT, gm, x, y, radius, points;
+int main()
+{
+    int gd = DETECT, gm, xc, yc, radius;
 
-    initgraph(&gd, &gm, "C://TURBOC3//BGI");
+    printf("Enter the center coordinates (xc yc): ");
+    scanf("%d %d", &xc, &yc);
 
-    printf("Enter radius: ");
+    printf("Enter the radius: ");
     scanf("%d", &radius);
 
-    printf("Enter number of points: ");
-    scanf("%d", &points);
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
 
-    printf("Enter coordinates (x y): ");
-    scanf("%d %d", &x, &y);
+    midPointCircle(xc, yc, radius);
 
-    drawCircle(x, y, radius, points);
-
-    getch();  // Wait for a key press before closing the graphics window
-
+    getch();
     closegraph();
     return 0;
 }
