d1e480934349f92df50036e54330b47d404beb8d
codemaster <145989639+codemater1234@users.noreply.github.com>
2023-11-18 15:08:38 +0530

    Update BOUNDARY.C

--- BOUNDARY.C
+++ BOUNDARY.C
@@ -1,30 +1,28 @@
-#include <graphics.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include<dos.h>
-
-void boundaryFill(int x, int y, int fillColor, int boundaryColor) {
-    if (getpixel(x, y) != boundaryColor && getpixel(x, y) != fillColor) {
-        putpixel(x, y, fillColor);
-        boundaryFill(x + 1, y, fillColor, boundaryColor);
-        boundaryFill(x - 1, y, fillColor, boundaryColor);
-        boundaryFill(x, y + 1, fillColor, boundaryColor);
-        boundaryFill(x, y - 1, fillColor, boundaryColor);
-    }
-}
-
-int main() {
-    int gd = DETECT, gm;
-    int x = 100, y = 100;
-    int fillColor = RED;
-    int boundaryColor = WHITE;
-
-    initgraph(&gd, &gm, "C:\\Turboc3\\BGI");
-    rectangle(50, 50, 200, 150);
-
-    boundaryFill(x, y, fillColor, boundaryColor);
-
-    delay(5000);
-    closegraph();
-    return 0;
-}
+#include<stdio.h>
+ #include<stdlib.h>
+ #include<conio.h>
+ #include<graphics.h>
+ #include<dos.h>
+ void boundaryfill(int x, int y, int boundary, int fill)
+ {
+ if((getpixel(x,y) != boundary) && (getpixel(x,y) != fill))
+ {
+ setcolor(fill);
+ putpixel(x,y,fill);
+ delay(5);
+ boundaryfill(x+1,y,fill,boundary);
+ boundaryfill(x-1,y,fill,boundary);
+ boundaryfill(x,y+1,fill,boundary);
+ boundaryfill(x,y-1,fill,boundary);
+ }
+ }
+ void main()
+ {
+ int gd= DETECT,gm;
+ initgraph(&gd,&gm,"C://TURBOC3//BGI");
+ setcolor(10);
+ rectangle(250,200,310,260);
+ boundaryfill(280,250,12,10);
+ getch();
+ closegraph();
+ }
