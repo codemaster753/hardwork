98f6cdd00ef226196ff1edf71ba260ee3b1d76e4
codemaster <145989639+codemaster753@users.noreply.github.com>
2023-11-19 11:15:10 +0530

    Update BEZEIR.C

--- BEZEIR.C
+++ BEZEIR.C
@@ -13,7 +13,7 @@ void bezier(int x[4], int y[4])
 
     for (t = 0.0; t < 1.0; t += 0.0005)
     {
-        double xt = pow(1 - t, 3) * x[0] + 3 * t * pow(1 - t, 2) * x[1] + 3 * pow(t, 2) * (1 - t) * x[2] + pow(t, 3) * x[3];
+        double xt = pow(1 - t, 3) * x[0] + 3 * t * pow(1 - t, 2) * x[1] + 3 * pow(t, 2) * (1 - t) * x[2] + pow(t, 3) * x[3];  
         double yt = pow(1 - t, 3) * y[0] + 3 * t * pow(1 - t, 2) * y[1] + 3 * pow(t, 2) * (1 - t) * y[2] + pow(t, 3) * y[3];
 
         putpixel((int)xt, (int)yt, WHITE); // Convert double to int for putpixel
